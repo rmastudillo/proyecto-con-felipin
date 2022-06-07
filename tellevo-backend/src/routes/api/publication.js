c971cb025282dfb9c1561d26ff78043f38e7ae54
Felipe <fplois@uc.cl>
2022-06-07 19:08:15 -0400

    Endpoints create, read and delete

--- tellevo-backend/src/routes/api/publication.js
+++ tellevo-backend/src/routes/api/publication.js
@@ -9,11 +9,42 @@ const db = require('../../models');
 
 // http://localhost:3000/publication/ (GET para obtener todas las publicaciones)
 router.get('/', async (req, res) => {
-  const publications = await db.publication.findAll();
-  console.log("Holaaaaaaaa", publications);
-  res.send({ mensaje: publications });
-})
+  try {
+    const publications = await db.publication.findAll();
+    res.send({ publications: publications });
+  } catch {
+    res.status(400);
+  }
+});
 
+// http://localhost:3000/publication/ (GET para obtener publicacion por id)
+router.get('/:id', async (req, res) => {
+  const publication = await db.publication.findOne({where: {id : req.params.id}});
+  if (publication === null) {
+    res.send({ mensaje: "No existe una publicación con id = " + req.params.id});
+  }
+  res.send({ publication: publication });
+});
+
+// http://localhost:3000/publication/ (POST para crear una publicacion)
+router.post('/', async (req, res) => {
+  try {
+    const new_publication = await db.publication.create(req.body);
+    res.send({ publication: new_publication });  
+  } catch {
+    res.status(400);
+  }
+});
+
+// http://localhost:3000/publication/ (DELETE para eliminar una publicacion por id)
+router.delete('/:id', async (req, res) => {
+  const publication = await db.publication.findOne({where: {id : req.params.id}});
+  if (publication === null) {
+    res.send({ mensaje: "No existe una publicación con id = " + req.params.id});
+  }
+  await publication.destroy();
+  res.send({ mensaje: "Publicación eliminada" });
+});
 
 /* Aqui exporto al router */
 module.exports = router;
